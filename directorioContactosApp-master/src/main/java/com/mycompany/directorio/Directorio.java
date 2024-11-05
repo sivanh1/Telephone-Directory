2b053653806dc0c686131cdbd8ce270675be87a5
sivanh1 <154664183+sivanh1@users.noreply.github.com>
2024-11-05 22:17:42 +0530

    Add files via upload

---
+++ directorioContactosApp-master/src/main/java/com/mycompany/directorio/Directorio.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.directorio;
+
+/**
+ *
+ * @author Katherine Ospina
+ */
+public class Directorio {
+
+    public static void main(String[] args) {
+        
+        Contactoform otherpage = new Contactoform();
+        otherpage.setVisible(true);
+        
+        
+    }
+}
